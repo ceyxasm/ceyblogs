6029e9238406f20c47b87fe3b89398cde7dcbd47
ceyxasm <abumallikmail@gmail.com>
2022-10-05 00:36:12 +0530

    pages added

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from "gatsby";
+
+export default function AboutPage()
+{
+	return(
+		<main>
+			<h2> this is the about page </h2>
+			<Link to="/">Back to main </Link>
+		</main>
+	);
+}
---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Link, useStaticQuery, graphql } from "gatsby";
+
+export default function IndexPage() 
+{
+	const data = useStaticQuery(graphql`
+	query getTitle{
+	   site{
+	      siteMetadata{
+		title
+	      }
+	   }
+	}
+		`);
+
+	const meta = data?.site?.siteMetadata ?? {};
+
+	return (
+		<>
+		<header>
+			<Link to="/"> {meta.title} </Link>
+		</header>
+
+		<main>
+		<h1> hello this is my page of blog. </h1>
+		<h2>hoping render is real-time </h2>
+		<Link to="/about">About page </Link>
+		</main>
+		</>
+	);
+}
+
